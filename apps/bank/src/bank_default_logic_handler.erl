ebd612572436f6f86c56fccc5adf2404c9845f4a
Juan Facorro <juan.facorro@klarna.com>
2020-10-18 19:59:28 +0200

    Dummy handler code for all endpoints

--- apps/bank/src/bank_default_logic_handler.erl
+++ apps/bank/src/bank_default_logic_handler.erl
@@ -11,7 +11,55 @@
     Context :: #{}
 ) ->
     {Status :: cowboy:http_status(), Headers :: cowboy:http_headers(), Body :: jsx:json_term()}.
-
+handle_request('GetAccount', _Req, _Context) ->
+    Body = #{ id => <<"ID">>
+            , account_holder_id => <<"ID">>
+            , balance => 100
+            },
+    {200, #{}, Body};
+handle_request('CreateAccount', _Req, _Context) ->
+    Body = #{ id => <<"ID">>
+            , account_holder_id => <<"ID">>
+            , balance => 100
+            },
+    {201, #{}, Body};
+handle_request('GetAccountHolder', _Req, _Context) ->
+    Body = #{ id => <<"ID">>
+            , first_name => <<"First">>
+            , last_name => <<"Last">>
+            },
+    {200, #{}, Body};
+handle_request('CreateAccountHolder', _Req, _Context) ->
+    Body = #{ id => <<"ID">>
+            , first_name => <<"First">>
+            , last_name => <<"Last">>
+            },
+    {201, #{}, Body};
+handle_request('GetTransfer', _Req, _Context) ->
+    Body = #{ id => <<"ID">>
+            , source_account_id => <<"ID">>
+            , destination_account_id => <<"ID">>
+            , amount => 100
+            , status => <<"CREATED">>
+            },
+    {200, #{}, Body};
+handle_request('CreateTransfer', _Req, _Context) ->
+    Body = #{ id => <<"ID">>
+            , source_account_id => <<"ID">>
+            , destination_account_id => <<"ID">>
+            , amount => 100
+            , status => <<"CREATED">>
+            },
+    {201, #{}, Body};
+handle_request('UpdateTransfer', _Req, _Context) ->
+    Body = #{ id => <<"ID">>
+            , status => <<"CREATED">>
+            },
+    {201, #{}, Body};
+handle_request('Ping', _Req, _Context) ->
+    {200, #{}, #{}};
+handle_request('Healthcheck', _Req, _Context) ->
+    {200, #{}, #{healthy => [], unhealthy => []}};
 handle_request(OperationID, Req, Context) ->
     error_logger:error_msg(
         "Got not implemented request to process: ~p~n",
