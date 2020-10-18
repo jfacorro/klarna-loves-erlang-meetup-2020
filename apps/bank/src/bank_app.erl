3639739c86919fecdf1ace577be4f212042a3368
Juan Facorro <juan.facorro@klarna.com>
2020-10-18 15:23:15 +0200

    Start HTTP server when application starts

--- apps/bank/src/bank_app.erl
+++ apps/bank/src/bank_app.erl
@@ -10,6 +10,8 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Opts = #{ip => {127,0,0,1}, port => 8080, net_opts => []},
+    bank_server:start(bank_http_server, Opts),
     bank_sup:start_link().
 
 stop(_State) ->
